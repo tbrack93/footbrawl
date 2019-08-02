a4a832c2ca710fedf2ff7ae26df99fb2bbd0d997
Tim Brackfield <txb846@cs.bham.ac.uk>
2019-08-02 15:22:49 +0100

    more improvements

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -384,14 +384,7 @@ function decodeMessage(message){
 	    }else if(message.action == "REROLLCHOICE"){
 			showRerollUsed(message);
 		} else if(message.action == "BLOCKOVER"){
-			var task = function(m){
-				 setTimeout(function(){
-					   console.log("Block ended");
-					   showBlockEnd(message);
-				   } , 2000); 
-	    	  };
-	    	  var t = animateWrapFunction(task, this, [message]);
-	    	  taskQueue.push(t);
+			showBlockEnd(message);
 		}else if(message.action == "SKILLUSED"){
 			console.log("skill used");
 			if(animating == true){
@@ -446,6 +439,7 @@ function decodeMessage(message){
 		    	  var t = animateWrapFunction(task, this, [message]);
 		    	  taskQueue.push(t);
 		      } else{ 	
+		    	  message.alert = true;
 		    	  showNewTurn(message); 
 		      }
 		} else if(message.action == "TOUCHDOWN"){
@@ -1218,6 +1212,9 @@ function showNewTurn(message){
 	document.getElementById("team2Name").innerHTML = message.team2Name;
 	document.getElementById("activeTeam").innerHTML = teamName;
 	turnover = false;
+	if(message.alert == true){
+		alert(teamName);
+	}
 	document.getElementById("score").innerHTML = ""+ message.team1Score + " - " + message.team2Score;
 	document.getElementById("team2Turn").innerHTML = "Current Turn: " + team2.turn;
 	document.getElementById("team1Turn").innerHTML = "Current Turn: " + team1.turn;
@@ -1433,6 +1430,7 @@ function showSkillUsed(message){
 }
 
 function showBlockEnd(message){
+	document.getElementById("modalImages").html = "";
 	document.getElementById("closeModal").style.display = "block";
 	inModal = false;
 	inBlock = false;
