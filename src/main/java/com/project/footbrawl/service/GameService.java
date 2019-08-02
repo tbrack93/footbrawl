72cc38d0eb11ba979c9fee51e43f1a4bd83f1ee8
Tim Brackfield <txb846@cs.bham.ac.uk>
2019-08-02 17:25:50 +0100

    chain push working as expected

--- src/main/java/com/project/footbrawl/service/GameService.java
+++ src/main/java/com/project/footbrawl/service/GameService.java
@@ -33,7 +33,7 @@ public class GameService {
 	MessageSendingService sender;
 
 	private static List<Integer> diceRolls = new ArrayList<>(
-			Arrays.asList(new Integer[] { 1, 4, 1, 1, 6, 1, 1, 1, 1, 6, 6, 4, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6}));
+			Arrays.asList(new Integer[] { 1, 4, 6, 6, 6, 6, 6, 1, 1, 6, 6, 4, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6}));
 	private static boolean testing = true;
 
 	// needed for finding neighbouring tiles
